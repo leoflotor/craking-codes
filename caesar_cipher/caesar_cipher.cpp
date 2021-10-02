dc140d17ae822a855e9f743a729315358a7053c1
leoflotor <leonardoflotor@gmail.com>
2021-10-01 21:28:51 -0500

    Functions to ask for message, mode and key

--- caesar_cipher/caesar_cipher.cpp
+++ caesar_cipher/caesar_cipher.cpp
@@ -1,45 +1,76 @@
-/* #include <bits/stdc++.h> */ // Apparently this is used for competitive
-                               // programming
 #include <algorithm>
 #include <iostream>
 #include <string>
 
-int main() {
-  std::string uppercase;
-  std::string lowercase;
-  std::string numbers;
-  std::string extra;
-  std::string symbols;
-  std::string message;
-  int key;
-  int position;
+/* Usr input variables */
+std::string message;
+std::string mode;
+int key;
 
-  uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  lowercase = "abcdefghijklmnopqrstuvwxyz";
-  numbers = "1234567890";
-  extra = " !?";
+/* Available characters */
+std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+std::string numbers = "1234567890";
+std::string extra = " ,.:;!?";
+std::string symbols = lowercase + uppercase + numbers + extra;
 
-  symbols = lowercase + uppercase + numbers + extra;
+/* Ask for encryption/decryption mode */
+void ask_mode() {
+  std::cout << "Select mode encrypt[enc] / decrypt[dec]: ";
+  std::getline(std::cin, mode);
 
-  /* Ask for the message to be encrypted/decrypted */
-  std::cout << "Write down your message: ";
-  std::getline(std::cin, message);
-
-  /* Enter key */
-  std::cout << "Enter encryption/decryption key: ";
-  std::cin >> key;
-
-  /* To allow negative keys */
-  if (key < 0) {
-    reverse(symbols.begin(), symbols.end());
-    key = -key;
+  if (mode != "enc" && mode != "dec") {
+    std::cout << "Select a valid mode." << std::endl;
+    ask_mode();
   }
 
-  /* Encryption/decryption */
-  for (int i = 0; i < message.length(); ++i) {
-    if (symbols.find(message[i]) != std::string::npos) {
-      position = (symbols.find(message[i]) + key) % symbols.length();
-      message[i] = symbols[position];
+  if (mode == "dec") {
+    reverse(symbols.begin(), symbols.end());
+  }
+
+  return;
+}
+
+void ask_message() {
+  std::cout << "Write down your message (MAX length: " << symbols.length()
+            << " charcters): ";
+  std::getline(std::cin, message);
+
+  /* Maximum length of the message */
+  if (message.length() > 100) {
+    std::cout << "Invalid message length." << std::endl;
+    ask_message();
+  }
+
+  return;
+}
+
+void ask_key() {
+  std::cout << "Enter encryption/decryption key [1 - " << symbols.length()
+            << "] : ";
+  std::cin >> key;
+
+  if (key > symbols.length() || key <= 0) {
+    std::cout << "Enter a valid key." << std::endl;
+    ask_key();
+  };
+
+  return;
+}
+
+int main() {
+  int position;
+
+  /* Call the functions to enter the required information  */
+  ask_message();
+  ask_mode();
+  ask_key();
+
+  /* Use a ranged-loop to encrypt/decrypt all the characters of the message */
+  for (auto &character : message) {
+    if (symbols.find(character) != std::string::npos) {
+      position = (symbols.find(character) + key) % symbols.length();
+      character = symbols[position];
     }
   }
 
